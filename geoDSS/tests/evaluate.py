46a5551f94f0ea94a7e26e40fb0acd728961aa4d
deduikertjes <deduikertjes@xs4all.nl>
2017-12-03 20:18:00 +0100

    added a boolean evaluation rule to apply logic to executed rules (lik if rules.this and rules.that report bla bla)

---
+++ geoDSS/tests/evaluate.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+from ..tests.test import test
+
+class evaluate(test):
+    '''
+    This test does a boolean evaluation on two or more already executed rules.
+    This is usefull for combining tests.
+
+    definition is expected to be a dict having at least:
+        expression (string):                The expression to evaluate. Already executed tests must be preceded by "this".
+                                            eg. this.first_test and this.second_test
+                                            Between the named tests python boolan logic and operators can be used like "and", "or", "not"
+        report_template (format string):    Python format string with markdown support to be reported
+        add_to_report:                      a list of all named test of which the reports should be added to the report of this test
+
+    a useful yaml snippet for this test would be:
+            rules:
+            - evaluate_tests_1:
+                type: tests.evaluate
+                title: evaluating 'and'
+                description: test the evaluate test by doing an unit_test_1 and unit_test_2
+                expression: "rules.unit_test_1 and rules.unit_test_2"
+                report_template: unit_test_1 and unit_test_2 is true
+
+    a useful subject for this would be:
+        subject = {'result': True}
+    '''
+
+    def execute(self, subject):
+        ''' 
+        Executes the test.
+
+        subject is expected a dict (contents doesn't matter)
+        '''
+
+        self.logger.debug('Evaluating: %s' % self.definition["expression"]) 
+
+        try:
+            rules = self.rules
+            self.decision = eval( self.definition["expression"] )
+        except Exception as error:
+            return self._handle_execution_exception(subject, "Could not succesfully evaluate expression: '%s' with error: %s " % ( self.definition["expression"], str(error)))
+
+        if self.decision:
+            self.result.append(self.definition["report_template"])
+
+        if 'add_to_report' in self.definition:
+            for test_name in self.definition['add_to_report']:
+                self.result.extend(getattr(getattr(rules,test_name), 'result'))
+        
+        self.executed = True
+
+        return self._finish_execution(subject)
